a828587a507b3068ad693601ef8ceda363f70110
Eldaroth <flavio.waelchli@gmail.com>
2018-07-29 13:06:52 +0200

    - Added and edited various comments for better understanding of the code - Implemented exception handling for the MainMenu & InstrumentCatalogUI in case a user does not enter an int variable

--- src/com/music4you/ui/InstrumentCatalogUI.java
+++ src/com/music4you/ui/InstrumentCatalogUI.java
@@ -1,92 +1,125 @@
 package com.music4you.ui;
 
-import java.awt.*;
-import java.util.InputMismatchException;
 import java.util.Scanner;
+
+/**
+ * Creates and shows the submenu for the instruments catalog
+ *
+ * @author Eldaroth
+ * @version 1.0
+ */
 
 public class InstrumentCatalogUI {
 
+    /**
+     * Responsible for executing the program
+     */
     public void execute() {
         InstrumentCatalogUI.showMenu();
     }
 
+    /**
+     * Creates the entry menu for the instruments catalog
+     */
     public static void showMenu() {
         Scanner showMenu = new Scanner(System.in);
 
-        while (true) {
-            MenuSkeleton subMain = new MenuSkeleton("Instrument catalog", "Back to Main Menu");
-            subMain.addMenuItem("Add instrument");
-            subMain.addMenuItem("Search in catalog");
-            subMain.printMenu();
+        //while (true) {
+            try { // catches the exception if the user does not enter an int variable
+                MenuSkeleton subMain = new MenuSkeleton("Instrument catalog", "Back to Main Menu");
+                subMain.addMenuItem("Add instrument");
+                subMain.addMenuItem("Search in catalog");
+                subMain.printMenu();
 
-            int chosenOption = showMenu.nextInt();
+                int chosenOption = showMenu.nextInt();
 
-            switch (chosenOption) {
+                switch (chosenOption) {
 
-                case 1:
-                    System.out.println("\nIn development");
-                    break;
+                    case 1:
+                        System.out.println("\nIn development");
+                        break;
 
-                case 2:
-                    instrumentSearchFor();
-                    break;
+                    case 2:
+                        instrumentSearchFor();
+                        break;
 
-                case 0:
-                    MainMenu back = new MainMenu();
-                    back.showMain();
-                    break;
+                    case 0:
+                        //MainMenu back = new MainMenu();
+                        MainMenu.showMain();
+                        break;
 
-                default:
-                    System.out.println("\n \n \nPlease chose a valid option");
+                    default:
+                        System.out.println("\n \n \nPlease chose a valid option");
+                }
+
+            } catch (Exception e) {
+                System.out.println("\n \n \nInvalid Input. Please enter a number.");
+                showMenu();
             }
-        }
+        //}
     }
 
+    /*
+     * Creates the submenu for searching an instrument in the catalog
+     */
     public static void instrumentSearchFor() {
         Scanner searchFor = new Scanner(System.in);
 
-        while (true) {
-            MenuSkeleton subSearch = new MenuSkeleton("Search Instrument catalog", "Back to previous menu");
-            subSearch.setOptionalText("I'd like to search for:");
-            subSearch.addMenuItem("Inventory ID");
-            subSearch.addMenuItem("Model number");
-            subSearch.addMenuItem("Type of instrument");
-            subSearch.addMenuItem("Manufacturer");
-            subSearch.addMenuItem("Show all instruments");
-            subSearch.printMenu();
+        //while (true) {
+            try { // catches the exception if the user does not enter an int variable
+                MenuSkeleton subSearch = new MenuSkeleton("Search Instrument catalog", "Back to previous menu");
+                subSearch.setOptionalText("I'd like to search for:");
+                subSearch.addMenuItem("Inventory ID");
+                subSearch.addMenuItem("Model number");
+                subSearch.addMenuItem("Type of instrument");
+                subSearch.addMenuItem("Manufacturer");
+                subSearch.addMenuItem("Show all instruments");
+                subSearch.printMenu();
 
-            //int chosenOption = searchFor.nextInt();
+                int chosenOption = searchFor.nextInt();
 
-            switch (subSearch.userSelection(searchFor)) {
+                switch (chosenOption) {
 
-                case 1:
-                    System.out.println("\nIn development");
-                    break;
+                    case 1:
+                        System.out.println("\nIn development");
+                        break;
 
-                case 2:
-                    System.out.println("\nIn development");
-                    break;
+                    case 2:
+                        System.out.println("\nIn development");
+                        break;
 
-                case 3:
-                    System.out.println("\nIn development");
-                    break;
+                    case 3:
+                        System.out.println("\nIn development");
+                        break;
 
-                case 4:
-                    System.out.println("\nIn development");
-                    break;
+                    case 4:
+                        System.out.println("\nIn development");
+                        break;
 
-                case 5:
-                    System.out.println("\nIn development");
-                    break;
+                    case 5:
+                        System.out.println("\nIn development");
+                        break;
 
-                case 0:
-                    showMenu();
-                    break;
+                    case 0:
+                        showMenu();
+                        break;
 
-                default:
-                    System.out.println("\n \n \nPlease chose a valid option");
-                    break;
+                    default:
+                        System.out.println("\n \n \nPlease chose a valid option");
+                        break;
+                }
             }
-        }
+            catch (Exception e) {
+                System.out.println("\n \n \nInvalid Input. Please enter a number.");
+                showMenu();
+            }
+        //}
+    }
+
+    /**
+     * Adds an instrument to the catalog
+     */
+    public static void addInstrument() {
+        System.out.println("In development");
     }
 }
